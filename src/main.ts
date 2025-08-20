902d64cc3f8a83aabce28c1151932ca01701c4d0
namnx <namnx@muungroup.org>
2025-08-20 18:57:21 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,21 @@ import { GlobalExceptionFilter } from './exception-filters/global-exception.filt
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+
+  // chạy đúng port backend của bạn (4000) hoặc từ ENV
+  const port = Number(process.env.PORT ?? 4000);
+  await app.listen(port);
+  console.log(`API listening on http://localhost:${port}`);
 }
 bootstrap();
